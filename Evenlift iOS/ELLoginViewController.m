4d90751bbb356cbdf0422b9a233f52618fc0e148
Gianni Settino <gianni@wherecloud.com>
2014-03-22 00:43:53 +0100

    Start implementing Facebook login code

--- Evenlift iOS/ELLoginViewController.m
+++ Evenlift iOS/ELLoginViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ELLoginViewController.h"
+#import <FacebookSDK/FacebookSDK.h>
+#import <Firebase/Firebase.h>
+#import <FirebaseSimpleLogin/FirebaseSimpleLogin.h>
+
+#define kEvenliftURL @"https://evenlift.firebaseio.com/"
 
 @interface ELLoginViewController ()
+
+@property (nonatomic, strong) FirebaseSimpleLogin* authClient;
 
 @end
 
@@ -26,7 +33,32 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view from its nib.
+    Firebase* f = [[Firebase alloc] initWithUrl:kEvenliftURL];
+    self.authClient = [[FirebaseSimpleLogin alloc] initWithRef:f];
+    
+    [self.authClient checkAuthStatusWithBlock:^(NSError *error, FAUser *user) {
+        if (error) {
+            // there was an error
+            NSLog(@"ERROR");
+        } else if (!user) {
+            // user is not logged in
+            NSLog(@"USER NOT LOGGED IN");
+        } else {
+            // user is logged in
+            NSLog(@"USER LOGGED IN");
+        }
+    }];
 }
 
+- (IBAction)login:(id)sender {
+    [self.authClient loginToFacebookAppWithId:@"420007321469839" permissions:nil audience:ACFacebookAudienceFriends withCompletionBlock:^(NSError *error, FAUser *user) {
+        if (error) {
+            NSLog(@"FB LOGIN ERROR");
+        } else {
+            NSLog(@"FB LOGIN SUCCESS");
+        }
+    }];
+}
 @end
