7e4ec74851d44176337e317c1609176f683a41b5
Gianni Settino <gianni@wherecloud.com>
2014-04-01 10:43:07 +0200

    A little bit of organization + create ELWorkoutTableViewCell

---
+++ Evenlift iOS/Views/ELWorkoutTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ELWorkoutTableViewCell.h
+//  Evenlift iOS
+//
+//  Created by Gianni Settino on 2014-04-01.
+//  Copyright (c) 2014 Evenlift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELWorkoutTableViewCell : UITableViewCell
+
+@end
---
+++ Evenlift iOS/Views/ELWorkoutTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  ELWorkoutTableViewCell.m
+//  Evenlift iOS
+//
+//  Created by Gianni Settino on 2014-04-01.
+//  Copyright (c) 2014 Evenlift. All rights reserved.
+//
+
+#import "ELWorkoutTableViewCell.h"
+
+@implementation ELWorkoutTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
