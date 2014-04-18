c2617dfe17833166b63a4d4349cc3e8cbe6a25a7
Gianni Settino <gianni@wherecloud.com>
2014-04-18 13:56:02 +0200

    Create Settings viewController, delete Home, and organize project better

---
+++ Evenlift iOS/ELSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ELSettingsTableViewController.h
+//  Evenlift iOS
+//
+//  Created by Gianni Settino on 2014-04-18.
+//  Copyright (c) 2014 Evenlift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELSettingsTableViewController : UITableViewController
+
+@end
