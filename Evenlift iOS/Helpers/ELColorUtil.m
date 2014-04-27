5acd9632ef2acb12c304ccb569e997a8f9f24279
Gianni Settino <gianni@wherecloud.com>
2014-04-28 00:09:51 +0200

    Minor: added color codes in different formats

--- Evenlift iOS/Helpers/ELColorUtil.m
+++ Evenlift iOS/Helpers/ELColorUtil.m
@@ -26,25 +26,29 @@
 
 + (UIColor*)evenLiftRed
 {
-    // FLAT UI "ALIZARIN"
+    // FLAT UI "ALIZARIN" = #e74c3c
+    // R:231 G:76 B:60
     return [UIColor colorWithRed:0.906 green:0.298 blue:0.235 alpha:1.0];
 }
 
 + (UIColor*)evenLiftRedHighlighted
 {
-    // FLAT UI "POMEGRANATE"
+    // FLAT UI "POMEGRANATE" = #c0392b
+    // R:192 G:57 B:43
     return [UIColor colorWithRed:0.753 green:0.224 blue:0.169 alpha:1.0];
 }
 
 + (UIColor*)evenLiftBlack
 {
     // #222222
+    // R:34 G:34 B:34
     return [UIColor colorWithRed:0.133 green:0.133 blue:0.133 alpha:1];
 }
 
 + (UIColor*)evenLiftWhite
 {
     // #f7f7f7
+    // R:247 G:247 B:247
     return [UIColor colorWithRed:0.969 green:0.969 blue:0.969 alpha:1];
 }
 
